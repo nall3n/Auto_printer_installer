9c2daeabf439d0f3713f59cb8123694f03a60dcd
Bjorn <b_970118@hotmail.com>
2024-08-19 16:36:42 +0200

    added array with printers to add and check if printer exists

--- PRINTER_test.ps1
+++ PRINTER_test.ps1
@@ -2,8 +2,49 @@
 
 $ip = "192.168.0.37"
 $port_name = "percyskök"
+$printer_driver = "Generic / Text Only"
 $printer_name = "percys kök"
 
 
-Add-PrinterPort -Name $port_name -PrinterHostAddress $ip
-Add-Printer -DriverName "Generic / Text Only" -Name $printer_name -PortName $port_name
+# $printer_1 = @{
+#     "Name" = "Percy food"
+#     "Port_name " = "percy_food"
+#     "Ip" = "192.168.0.37"
+# }
+
+
+$test_array = @(
+    @{
+        "Name" = "Percy food"
+        "Port_name" = "percy_food"
+        "Ip" = "192.168.0.37"
+    },
+    @{
+        "Name" = "Percy bar"
+        "Port_name" = "percy_bar"
+        "Ip" = "192.168.0.38"
+    },
+    @{
+        "Name" = "Nilssons food"
+        "Port_name" = "nilssons_food"
+        "Ip" = "192.168.0.37"
+    }
+)
+
+foreach ($item in $test_array){
+    Write-Output $item
+    
+    Get-Printer -Name $item["Name"]
+
+    if (Get-Printer -Name $item["Name"]) {
+        Write-Output "printer exists"
+    } else {
+        Write-Output "Adding Printer" 
+        Add-PrinterPort -Name $item["Port_name"] -PrinterHostAddress $item["Ip"]
+        Add-Printer -DriverName $printer_driver -Name $item["Name"] -PortName $item["Port_name"]
+    }
+}
+
+
+# Add-PrinterPort -Name $port_name -PrinterHostAddress $ip
+# Add-Printer -DriverName $printer_driver -Name $printer_name -PortName $port_name
