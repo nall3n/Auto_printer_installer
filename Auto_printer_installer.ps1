a21c1076365b6e02f48cd71ef7f18e01a1b704b8
Bjorn <b_970118@hotmail.com>
2024-08-20 08:17:00 +0200

    Added check if IP was changed and reconfigures ip of printer. also changed name of files

--- Auto_printer_installer.ps1
+++ Auto_printer_installer.ps1
@@ -1,17 +1,5 @@
 
-
-$ip = "192.168.0.37"
-$port_name = "percyskök"
 $printer_driver = "Generic / Text Only"
-$printer_name = "percys kök"
-
-
-# $printer_1 = @{
-#     "Name" = "Percy food"
-#     "Port_name " = "percy_food"
-#     "Ip" = "192.168.0.37"
-# }
-
 
 $test_array = @(
     @{
@@ -27,24 +15,28 @@ $test_array = @(
     @{
         "Name" = "Nilssons food"
         "Port_name" = "nilssons_food"
-        "Ip" = "192.168.0.37"
+        "Ip" = "192.168.0.50"
     }
 )
 
 foreach ($item in $test_array){
-    Write-Output $item
     
-    Get-Printer -Name $item["Name"]
+    $p = Get-Printer -Name $item["Name"]
 
-    if (Get-Printer -Name $item["Name"]) {
+    if ($p) {
         Write-Output "printer exists"
+        $Port = Get-PrinterPort -Name $p.PortName
+        if ($Port.PrinterHostAddress -ne $item["Ip"]){
+            Write-Output $Port.PrinterHostAddress
+            Set-Printer -Name $item["Name"] -PortName "COM1:" # Changes port to remove old network port
+            Remove-PrinterPort -Name $p.PortName
+            Add-PrinterPort -Name $item["Port_name"] -PrinterHostAddress $item["Ip"]
+            Set-Printer -Name $item["Name"] -PortName $item["Port_name"]
+            Write-Output $item["Ip"]
+        }
     } else {
         Write-Output "Adding Printer" 
         Add-PrinterPort -Name $item["Port_name"] -PrinterHostAddress $item["Ip"]
         Add-Printer -DriverName $printer_driver -Name $item["Name"] -PortName $item["Port_name"]
     }
 }
-
-
-# Add-PrinterPort -Name $port_name -PrinterHostAddress $ip
-# Add-Printer -DriverName $printer_driver -Name $printer_name -PortName $port_name
